7ff1947278ba92017f7b8fcd7c728601e704308a
Sayan Baishya <saysayanbaishya@gmail.com>
2026-07-05 01:48:01 +0530

    Revert temporary data population in build script

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python generate_new_university.py || true
-python populate_all_data.py GTU || true
