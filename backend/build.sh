ef5ce90e9bcb82c3d05542efa76b56902d575d52
Sayan Baishya <saysayanbaishya@gmail.com>
2026-07-05 01:36:11 +0530

    Temporarily run data population during build

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Temporary data population for free tier deployment
+python generate_new_university.py || true
+python populate_all_data.py GTU || true
